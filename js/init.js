b8d180d7ddc97d4de2df50ba896b2344484d7e7f
klevla <v.klivetskaya@gmail.com>
2018-03-24 21:46:46 +0200

    latets-news footer

--- js/init.js
+++ js/init.js
@@ -2,5 +2,12 @@ $( function() {
     // Tabs init
     $( "#tabs-services" ).tabs();
     $( "#tabs-portfolio" ).tabs();
+    
+    // init slick carousel
+    $('.reviews-carousel').slick({
+      dots: true,
+      prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+      nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',
+    });
   } );
   
